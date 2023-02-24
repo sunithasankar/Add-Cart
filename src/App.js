9bacfac2e547c5f3619e6c5a4d91d2040b9e16f3
sunitha <v.sunithasankar@gmail.com>
2023-02-24 21:29:13 +0530

    Hi this is foodapp

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi Sunitha This is React Demo</h1>
+    </div>
+  );
+}
+
+export default App;
