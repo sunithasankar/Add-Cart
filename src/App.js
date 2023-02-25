51f5fd58c272abf68c8f2e3423ca36bf1b2ade5b
sunitha <v.sunithasankar@gmail.com>
2023-02-25 17:12:38 +0530

    file chenged

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useEffect} from 'react';
 
 function App() {
+  const [search,setSearch]= usestate("");
+  const submitHandler = e =>{
+    e.preventDefault();
+    console.log(search);
+  }
   return (
     <div>
       <center>
-      <h4>Food Recipe App</h4>
+      <h4>Food Recipe App</h4> <br/>
+      <form onSubmit={submitHandler}>
+        <input type="text" value={search} onChange={() => setSearch(e.target.value)}/> <br/>
+        <input type="submit" value="search" />
+
+
+
+      </form>
       </center>
     </div>
   );
